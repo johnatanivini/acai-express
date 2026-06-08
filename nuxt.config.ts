dd901fb6ffe43199261b428dfb8d035c3b98bef5
ivinidev <johnatan.ivini@gmail.com>
2026-06-08 19:07:26 -0300

    First configuration NuxtJs

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+ css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
